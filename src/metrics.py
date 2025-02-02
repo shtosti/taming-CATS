b23614507f26dacd34cceeb5798ab8ce2c9cd4dc
shtosti <hanna.hubarava@gmail.com>
2025-02-02 21:45:49 +0100

    finalize metrics class

--- src/metrics.py
+++ src/metrics.py
@@ -1,58 +1,84 @@
 import os
 import pandas as pd
-import spacy
-# import textstat
-# from lexicalrichness import LexicalRichness
+import textstat
+from nltk.tokenize import sent_tokenize
 
 
 class Metrics:
 
-    def __init__(self, input_text, language='en'):
+    def __init__(self, input_text):
         self.text = input_text
-        self.language = language
-        # self.data_df = pd.DataFrame()
-        if self.language == "en": # default value unless overwritten
-            self.nlp = spacy.load("en_core_web_sm")
-        elif self.language == "es":
-            self.nlp = spacy.load("es_core_web_sm")
 
     def count_words(self) -> int:
         """
-        Count the number of words in the input sentence/text.
+        Counts the number of words in the input sentence/text.
         """
         return len(self.text.split())
     
     def count_chars(self) -> int:
         """
-        Count the number of all characters in the input sentence/text.
+        Counts the number of all characters in the input sentence/text.
         Includes both alphanumeric chars and punction, etc.
         """
         return len(self.text)
     
-    def count_alpha(self) -> int:
+    def count_alphanum(self) -> int:
         """
-        Count the number of alphanumeric characters in the input.
+        Counts the number of alphanumeric characters in the input.
         """   
         count = 0
         for char in self.text:
             if char.isalpha():
                 count += 1
         return count
-    
-    def count_sentences_spacy(self) -> int:
-        """
-        Count the number of sentences using Spacy.
-        """
-        nlp = self.nlp
-        doc = nlp(self.text)
-        return len(list(doc.sents))
 
+    def count_sents_nltk(self) -> int:
+        """
+        Counts the number of sentences with NLTK.
+        """
+        return len(sent_tokenize(self.text))
+
+    def compute_fre(self) -> float:
+        """
+        Computes Flesh Reading Ease score.
+        """
+        return textstat.flesch_reading_ease(self.text)
+
+    def compute_ari(self) -> float:
+        """
+        Computes automated readability index score.
+        """
+        return textstat.automated_readability_index(self.text) 
+
+    def compute_fkgl(self) -> float:
+        """
+        Computes the Flesch-Kincaid Grade Level (FKGL) score using textstat.
+        """
+        return textstat.flesch_kincaid_grade(self.text)
+
+    def compute_metrics(self) -> dict:
+        return {
+            'word_count': self.count_words(),
+            'char_count': self.count_chars(),
+            'alphanum_count': self.count_alphanum(),
+            'sent_count_nltk': self.count_sents_nltk(),
+            'FRE': self.compute_fre(),
+            'ARI': self.compute_ari(),
+            'FKGL': self.compute_fkgl()
+        }
+    
 
 text = "Love and peace, but also whatever 1,2,3!! haha@."
 metrics = Metrics(text)
 print(text)
-print(metrics.count_chars())
-print(metrics.count_alpha())
-print(metrics.count_words())
-print(metrics.count_sentences_spacy())
+print(metrics.compute_metrics())
+# print(metrics.count_chars())
+# print(metrics.count_alphanum())
+# print(metrics.count_words())
+# print(metrics.count_sents_nltk())
+# print(metrics.compute_fre())
+# print(metrics.compute_ari())
+# print(metrics.compute_fkgl())
+
+
 
