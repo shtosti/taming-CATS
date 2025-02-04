2678981174ed59e245b62daf956ddfe37f53ee01
shtosti <hanna.hubarava@gmail.com>
2025-02-04 11:45:22 +0100

    update newsela class: include metadata into init

--- src/Dataset.py
+++ src/Dataset.py
@@ -13,6 +13,17 @@ class NewselaDataset(TSDataset):
         self.data_dir = data_dir
         self.metadata_path = metadata_path
         self.limit = limit
+        # dataset metainfo
+        self.dataset_name = "newsela"
+        self.domain = "news"
+        self.annotation = "human"
+        self.alignment_level = "text"
+        # relevant columns
+        self.grade_level = "grade_level"
+        self.simplification_version = "version"
+        self.text = "content"
+        self.language = "language"
+        self.grouping_tag = "slug"
 
     def load_data(self):
         metadata = pd.read_csv(self.metadata_path, sep=",")
@@ -36,42 +47,42 @@ class NewselaDataset(TSDataset):
         pass
 
 
-# NEWSELA_DIR_PATH = "./../datasets/newsela/newsela_article_corpus_2016-01-29/articles"
-# NEWSELA_METADATA_PATH = "./../datasets/newsela/newsela_article_corpus_2016-01-29/articles_metadata.csv"
+# # NEWSELA_DIR_PATH = "./../datasets/newsela/newsela_article_corpus_2016-01-29/articles"
+# # NEWSELA_METADATA_PATH = "./../datasets/newsela/newsela_article_corpus_2016-01-29/articles_metadata.csv"
 
-# dataset = NewselaDataset(NEWSELA_DIR_PATH, NEWSELA_METADATA_PATH, limit=10)
-# dataset.load_data()
-# print(dataset.data_df.head())  # Check if preprocessing worked
+# # dataset = NewselaDataset(NEWSELA_DIR_PATH, NEWSELA_METADATA_PATH, limit=10)
+# # dataset.load_data()
+# # print(dataset.data_df.head())  # Check if preprocessing worked
 
-# df = dataset.data_df
-# print(df.columns)
+# # df = dataset.data_df
+# # print(df.columns)
 
-# class WikiLargeDataset(TSDataset):
-#     def __init__(self, base_path, base_filename):
-#         super().__init__()
-#         self.base_path = base_path
-#         self.base_filename = base_filename
+# # class WikiLargeDataset(TSDataset):
+# #     def __init__(self, base_path, base_filename):
+# #         super().__init__()
+# #         self.base_path = base_path
+# #         self.base_filename = base_filename
 
-#     def load_data(self):
-#         valid_src_path = os.path.join(self.base_path, f"{self.base_filename}valid.src")
-#         valid_dst_path = os.path.join(self.base_path, f"{self.base_filename}valid.dst")
+# #     def load_data(self):
+# #         valid_src_path = os.path.join(self.base_path, f"{self.base_filename}valid.src")
+# #         valid_dst_path = os.path.join(self.base_path, f"{self.base_filename}valid.dst")
 
-#         with open(valid_src_path, "r") as src_file, open(valid_dst_path, "r") as dst_file:
-#             valid_src = src_file.readlines()
-#             valid_dst = dst_file.readlines()
+# #         with open(valid_src_path, "r") as src_file, open(valid_dst_path, "r") as dst_file:
+# #             valid_src = src_file.readlines()
+# #             valid_dst = dst_file.readlines()
 
-#         valid_df = pd.DataFrame({"source": valid_src, "target": valid_dst})
-#         self.data_df = valid_df[valid_df["source"].str.strip().astype(bool) & valid_df["target"].str.strip().astype(bool)]
+# #         valid_df = pd.DataFrame({"source": valid_src, "target": valid_dst})
+# #         self.data_df = valid_df[valid_df["source"].str.strip().astype(bool) & valid_df["target"].str.strip().astype(bool)]
 
 
-# class MedEASiDataset(TSDataset):
-#     def __init__(self, file_path):
-#         super().__init__()
-#         self.file_path = file_path
+# # class MedEASiDataset(TSDataset):
+# #     def __init__(self, file_path):
+# #         super().__init__()
+# #         self.file_path = file_path
 
-#     def load_data(self):
-#         self.data_df = pd.read_csv(self.file_path)
-#         self.data_df.rename(columns={"Expert": "source", "Simple": "target"}, inplace=True)
-#         self.data_df = self.data_df[
-#             self.data_df["source"].str.strip().astype(bool) & self.data_df["target"].str.strip().astype(bool)
-#         ]
+# #     def load_data(self):
+# #         self.data_df = pd.read_csv(self.file_path)
+# #         self.data_df.rename(columns={"Expert": "source", "Simple": "target"}, inplace=True)
+# #         self.data_df = self.data_df[
+# #             self.data_df["source"].str.strip().astype(bool) & self.data_df["target"].str.strip().astype(bool)
+# #         ]
