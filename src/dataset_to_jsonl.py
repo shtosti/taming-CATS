9f295ed9506adc73646bb8d8a0a70ed55da7bb5c
shtosti <hanna.hubarava@gmail.com>
2025-02-04 16:07:43 +0100

    remove comments

--- src/dataset_to_jsonl.py
+++ src/dataset_to_jsonl.py
@@ -20,10 +20,8 @@ def convert_to_jsonl(dataset, grouped_df):
     jsonl_data = []
 
     for slug, group in grouped_df:
-        # group = group.sort_values(by=dataset.grade_level)
         source_row = group[group[dataset.simplification_version] == 0]
         source_text = source_row.iloc[0][dataset.text]
-        # source_text = group.iloc[0][dataset.text]
         source_metrics = Metrics(source_text).compute_metrics()
 
         # iterate through all grade levels ~ simplifications
