499a6641d537e62f8dce5da584107df7e3fce070
shtosti <hanna.hubarava@gmail.com>
2025-02-16 23:01:55 +0100

    regenerate plots with correct metric axes

--- src/explore_datasets.py
+++ src/explore_datasets.py
@@ -69,8 +69,8 @@ def plot_compression(data, metric, dataset_dir):
     
 
 def get_compression_values(data, metric):
-    source_lengths = []
-    target_lengths = []
+    source_val_arr = []
+    target_val_arr = []
 
     for line in data:
 
@@ -78,16 +78,64 @@ def get_compression_values(data, metric):
         for simplification in simplifications:
             if metric in ["char", "sentence", "word"]:
                 target_value = simplification["target_metrics"][f"{metric}_count"]
-                target_lengths.append(target_value)
+                target_val_arr.append(target_value)
                 source_value = line["source_metrics"][f"{metric}_count"]
-                source_lengths.append(source_value)
+                source_val_arr.append(source_value)
             else:
                 target_value = simplification["target_metrics"][f"{metric}"]
-                target_lengths.append(target_value)
+                target_val_arr.append(target_value)
                 source_value = line["source_metrics"][f"{metric}"]
-                source_lengths.append(source_value)
+                source_val_arr.append(source_value)
     
-    return source_lengths, target_lengths
+    return source_val_arr, target_val_arr
+
+def get_eval_values(data, metric):
+    target_val_arr = []
+
+    for line in data:
+        simplifications = line["simplifications"]
+        for simplification in simplifications:
+            if metric in ["BLEU", "BERTScore"]:
+                target_value = simplification["target_metrics"][f"{metric}"]
+                target_val_arr.append(target_value)
+    
+    return target_val_arr
+
+def plot_eval_values(data, metric, dataset_dir):
+    target_vals = get_eval_values(data, metric)
+
+
+    # Plotting the distribution shift
+    # Calculate the common bin edges for source and target distributions
+    min_val = min(target_vals)
+    max_val = max(target_vals)
+    bins = np.linspace(min_val, max_val, 25)  # TODO adjust n bins to ompimize the visual effect 
+
+    # Plotting the distribution shift
+    plt.figure(figsize=(12, 6))
+
+    ############ histograms with aligned bins ############
+    plt.subplot(1, 2, 1)
+    plt.hist(target_vals, bins=bins, alpha=0.4, label="Simplification", color='green', density=True)
+    # Add KDE for smooth distribution curves
+    if len(set(target_vals)) > 1:
+        sns.kdeplot(target_vals, color='green', linewidth=1, label="Simplification KDE")
+    plt.xlabel(f'{metric}')
+    plt.ylabel('Frequency')
+    plt.legend()
+
+    ############ violin plot ############
+    plt.subplot(1, 2, 2)
+    sns.violinplot(x=target_vals, color='green', inner="quartile")
+    sns.boxplot(x=target_vals, color='black', width=0.15, fliersize=3)  # Add box plot for summary stats
+    plt.xlabel(f'{metric} Score')
+    plt.title(f'Distribution of {metric} Scores')
+    
+    # save plots
+    output_dir = f"{dataset_dir}/stats/visuals"
+    os.makedirs(output_dir, exist_ok=True)
+    plt.tight_layout()
+    plt.savefig(f"{output_dir}/{metric}.png", dpi=400)
     
 
 def main():
@@ -106,7 +154,7 @@ def main():
         DATASET_PATH = f"{DATASET_DIR}/dataset.jsonl"
 
         dataset = load_jsonl(DATASET_PATH)
-        # dataset = dataset[:500]
+        # dataset = dataset[:500] # for dev
         metrics_to_plot = [
                             "char", 
                             "word", 
@@ -118,6 +166,13 @@ def main():
                             ]
         for metric in metrics_to_plot:
             plot_compression(data=dataset, metric=metric, dataset_dir=DATASET_DIR)
+
+        eval_metrics_to_plot = [
+                                "BLEU",
+                                "BERTScore"
+                                ]
+        for metric in eval_metrics_to_plot:
+            plot_eval_values(data=dataset, metric=metric, dataset_dir=DATASET_DIR)
 
 
 if __name__ == "__main__":
