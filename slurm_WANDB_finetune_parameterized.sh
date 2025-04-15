64ecd8b9e0655ab6de6c0756409d2761c7ff0710
shtosti <hanna.hubarava@gmail.com>
2025-04-15 15:35:50 +0200

    reduce gpu and memory demands

--- slurm_WANDB_finetune_parameterized.sh
+++ slurm_WANDB_finetune_parameterized.sh
@@ -5,8 +5,8 @@
 #SBATCH --account=iict-sp1.ebling.cl.uzh
 #SBATCH --output=logs/%x_%j_out.log
 #SBATCH --error=logs/%x_%j_err.log 
-#SBATCH --gpus=A100:1
-#SBATCH --mem=80000
+#SBATCH --gpus=1
+#SBATCH --mem=32000
 #SBATCH --time=02:00:00              # Max runtime (hh:mm:ss)
 
 conda activate sft
