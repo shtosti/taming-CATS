c02655f235a72e9dc9e8913717e4d3e1e786c7af
shtosti <hanna.hubarava@gmail.com>
2025-04-16 14:46:12 +0200

    add peft logic

--- run_WANDB_finetune_parameterized.sh
+++ run_WANDB_finetune_parameterized.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# --- model family
+# --- model family # TODO
 MODEL_FAMILY="llama"
-MODEL_NAME="meta-llama/Llama-3.2-1B-Instruct"
-# MODEL_NAME="meta-llama/Meta-Llama-3-8B-Instruct"
+# MODEL_NAME="meta-llama/Llama-3.2-1B-Instruct"
+MODEL_NAME="meta-llama/Meta-Llama-3-8B-Instruct"
 
 # MODEL_FAMILY="auto"
 # MODEL_NAME="Qwen/Qwen2.5-1.5B-Instruct"
 # MODEL_NAME="ministral/Ministral-3b-instruct"
+
+# --- PEFT flag --- # TODO
+USE_PEFT=true 
 
 # --- dataset settings ---
 DATASET_NAME="Med-EASi"
@@ -15,15 +18,17 @@ SLICE_TRAIN="-1" # -1 means no slicing
 SLICE_VAL="-1" # -1 means no slicing
 
 # --- prompting settings ---
-PROMPTING_TYPE="vanilla" # "vanilla", "reasoning", "transformations"
-USER_PROMPT_ID="token_explanation_examples" # "token", "token_explanation", "token_explanation_examples"
+# "vanilla", "reasoning", "transformations"
+PROMPTING_TYPE="vanilla" 
+# "token", "token_explanation", "token_explanation_examples"
+USER_PROMPT_ID="token_explanation"
 
 # --- metric settings ---
 METRIC_NAME="FKGL"
 
 # --- hyperparameters ---
 EPOCHS=2
-BATCH_SIZE=8 # TODO increase to 8 or 16
+BATCH_SIZE=8
 GRADIENT_ACCUMULATION_STEPS=4
 LR=1e-5
 WEIGHT_DECAY=0.01
@@ -34,6 +39,12 @@ LOG_EVERY=40
 WANDB_PROJECT_NAME="thesis-SFT"
 WANDB_ENTITY="shtosti"
 
+if [ "$USE_PEFT" = true ]; then
+    echo "Using PEFT..."
+    PEFT_FLAG="--peft"  # Enable PEFT by passing the --peft flag
+else
+    PEFT_FLAG=""  # If not using PEFT, leave the flag empty
+fi
 
 CUDA_LAUNCH_BLOCKING=1 \
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
@@ -58,4 +69,5 @@ python src/WANDB_finetune_parameterized.py \
     --control_tokens "data/prompts/control_tokens.json" \
     --system_prompts "data/prompts/system_prompts.json" \
     --user_prompts "data/prompts/user_prompts.json"\
-    --log_every "$LOG_EVERY"
+    --log_every "$LOG_EVERY"\
+    $PEFT_FLAG
