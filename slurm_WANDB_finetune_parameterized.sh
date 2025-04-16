57c28b66fa7f8a85598094f72cd661f7e3e8c239
shtosti <hanna.hubarava@gmail.com>
2025-04-16 02:06:52 +0200

    max gpu and mem

--- slurm_WANDB_finetune_parameterized.sh
+++ slurm_WANDB_finetune_parameterized.sh
@@ -5,9 +5,9 @@
 #SBATCH --account=iict-sp1.ebling.cl.uzh
 #SBATCH --output=logs/%x_%j_out.log
 #SBATCH --error=logs/%x_%j_err.log 
-#SBATCH --gpus=1
-#SBATCH --mem=32000
-#SBATCH --time=02:00:00              # Max runtime (hh:mm:ss)
+#SBATCH --gpus=A100:1
+#SBATCH --mem=80000
+#SBATCH --time=03:00:00              # Max runtime (hh:mm:ss)
 
 conda activate sft
 module load gpu
