60d9d15403cdace66a8894f1f88e3599e1c0da18
shtosti <hanna.hubarava@gmail.com>
2025-04-16 13:26:23 +0200

    add cpus

--- slurm_WANDB_finetune_parameterized.sh
+++ slurm_WANDB_finetune_parameterized.sh
@@ -6,6 +6,7 @@
 #SBATCH --output=logs/%x_%j_out.log
 #SBATCH --error=logs/%x_%j_err.log 
 #SBATCH --gpus=A100:1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=80000
 #SBATCH --time=03:00:00              # Max runtime (hh:mm:ss)
 
