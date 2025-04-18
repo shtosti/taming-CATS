3efe709098431d2d56c0bd216c9bfb2844b769f7
shtosti <hanna.hubarava@gmail.com>
2025-04-18 13:17:59 +0200

    add func to create inference/val prompt

--- src/helpers/prompting.py
+++ src/helpers/prompting.py
@@ -69,3 +69,10 @@ def format_prompt_with_special_tokens(system_prompt, user_prompt):
 
 def format_completion_with_special_tokens(completion):
     return f"{completion} <|eot_id|>"
+
+def create_inference_prompt(text, metric_value, metric_name):
+    return (
+        f"<|begin_of_text|><|start_header_id|>user<|end_header_id|> "
+        f"<{metric_name}={metric_value}> {text} <|eot_id|>\n"
+        f"<|start_header_id|>assistant<|end_header_id|> "
+    )
