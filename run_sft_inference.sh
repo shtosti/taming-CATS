6e1ce41081632665d41459e657b562837e4c21d3
shtosti <hanna.hubarava@gmail.com>
2025-04-29 15:37:27 +0200

    save output as json

--- run_sft_inference.sh
+++ run_sft_inference.sh
@@ -4,11 +4,14 @@ MODEL_PATH="sft/Llama-3.2-1B-Instruct-Med-EASi-20250423-1516-l9qlzepa"
 USER_PROMPT_ID="token_explanation"
 METRIC_NAME="ARI"
 DATASET_NAME="Med-EASi"
-
 # "llama", "mistral", "qwen", "base" (for prompt template)
 MODEL_FAMILY="base"
 
 
+
+OUTPUT_DIR="output/$MODEL_PATH"
+OUTPUT_FILE="$OUTPUT_DIR/output.json"
+mkdir -p "$OUTPUT_DIR"  # Ensure the directory exists
 
 python src/sft_inference.py \
   --model_path "$MODEL_PATH"\
@@ -17,8 +20,8 @@ python src/sft_inference.py \
   --model_family "$MODEL_FAMILY" \
   --max_length 512 \
   --batch_size 4 \
-  --slice_test -1 \
-  --output_file predictions.txt \
+  --slice_test 20 \
+  --output_file "$OUTPUT_FILE" \
   --control_tokens "data/prompts/control_tokens.json" \
   --system_prompts "data/prompts/system_prompts.json" \
   --user_prompts "data/prompts/user_prompts.json" \
