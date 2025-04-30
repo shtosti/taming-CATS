344d4e74ae57c15b6e6efcec93d9d50f003dcd2d
shtosti <hanna.hubarava@gmail.com>
2025-04-30 19:01:07 +0200

    add output dir

--- run_sft_eval.sh
+++ run_sft_eval.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-INPUT_FILE="output/sft/Llama-3.2-1B-Instruct-Med-EASi-20250423-1516-l9qlzepa/output.json"
+INPUT_DIR="output/sft/Llama-3.2-1B-Instruct-Med-EASi-20250423-1516-l9qlzepa"
+INPUT_FILE="$INPUT_DIR/output.json"
 METRIC_NAME="ARI"
+
+
+
+OUTPUT_DIR=$INPUT_DIR
 
 python src/sft_eval.py \
   --input_file "$INPUT_FILE" \
   --metric_key "$METRIC_NAME" \
+  --output_dir "$OUTPUT_DIR" \
