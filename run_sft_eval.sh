eda8e63ae0fcdfc675168f259612ef7328b5c525
shtosti <hanna.hubarava@gmail.com>
2025-05-06 21:08:26 +0200

    run upgraded eval

--- run_sft_eval.sh
+++ run_sft_eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INPUT_DIR="output/models/Llama-3.2-1B-Instruct-Med-EASi-WORD_COMPRESSION-base-token_explanation-20250505-1433-job7wghr"
+INPUT_DIR="output/models/Llama-3.2-1B-Instruct-Med-EASi-DALE-CHALL-base-token_explanation-20250505-1317-oqxznih4"
 INPUT_FILE="$INPUT_DIR/output.json"
-METRIC_NAME="WORD_COMPRESSION"
+METRIC_NAME="DALE-CHALL"
 
 
 
