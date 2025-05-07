66d6b4689590f94225c589258edef0c50a9ac60c
shtosti <hanna.hubarava@gmail.com>
2025-05-07 17:16:20 +0200

    run evals with added correlations

--- run_sft_eval.sh
+++ run_sft_eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INPUT_DIR="output/models/Llama-3.2-1B-Instruct-Med-EASi-ARI-base-token_explanation-20250505-1427-3kmdipyx"
+INPUT_DIR="output/models/Qwen2.5-1.5B-Instruct-Med-EASi-FKGL-token_explanation_examples-20250430-2050-bo5uv8tc"
 INPUT_FILE="$INPUT_DIR/output.json"
-METRIC_NAME="ARI"
+METRIC_NAME="FKGL"
 
 
 
