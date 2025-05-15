c1bf4a6df306eb3a721d28d1b0c84ee690267c0f
shtosti <hanna.hubarava@gmail.com>
2025-05-15 14:58:10 +0200

    more finetuning runs

--- run_sft_finetune.sh
+++ run_sft_finetune.sh
@@ -9,8 +9,8 @@ MODEL_CLASS="llama"
 MODEL_FAMILY="base"
 
 # --- model name
-MODEL_NAME="meta-llama/Llama-3.2-1B-Instruct"
-# MODEL_NAME="meta-llama/Meta-Llama-3-8B-Instruct"
+# MODEL_NAME="meta-llama/Llama-3.2-1B-Instruct"
+MODEL_NAME="meta-llama/Meta-Llama-3-8B-Instruct"
 # MODEL_NAME="meta-llama/Llama-2-13b-chat-hf"
 # MODEL_NAME="Qwen/Qwen2.5-1.5B-Instruct"
 # MODEL_NAME="Qwen/Qwen2.5-14B-Instruct"
@@ -18,7 +18,7 @@ MODEL_NAME="meta-llama/Llama-3.2-1B-Instruct"
 # MODEL_NAME="mistralai/Mistral-7B-Instruct-v0.1"
 
 # --- PEFT flag: set true for models above e.g. 2B
-USE_PEFT=false 
+USE_PEFT=true 
 
 # *** **** ***
 # *** **** ***
@@ -26,8 +26,8 @@ USE_PEFT=false
 
 # --- dataset settings ---
 DATASET_NAME="Med-EASi"
-SLICE_TRAIN="50" # -1 means no slicing
-SLICE_VAL="50" # -1 means no slicing
+SLICE_TRAIN="-1" # -1 means no slicing
+SLICE_VAL="-1" # -1 means no slicing
 
 # --- prompting settings ---
 # "vanilla", "reasoning", "transformations"
--- run_sft_inference.sh
+++ run_sft_inference.sh
@@ -2,20 +2,27 @@
 
 
 # ========== inference =========
-MODEL_PATH="models/Llama-3.2-1B-Instruct-Med-EASi-ARI-base-token_explanation-20250512-1705-zh8jna8l"
+MODEL_PATH="models/Meta-Llama-3-8B-Instruct-Med-EASi-FKGL-base-token_explanation-20250514-2147-u4v4gkmz"
 USER_PROMPT_ID="token_explanation"
-METRIC_NAME="ARI"
+METRIC_NAME="FKGL"
 DATASET_NAME="Med-EASi"
 # "llama", "mistral", "qwen", "base" (for prompt template)
 MODEL_FAMILY="base"
 # "llama" or "auto"
 MODEL_CLASS="llama"
-
+USE_PEFT=true
 
 
 OUTPUT_DIR="output/$MODEL_PATH"
 OUTPUT_FILE="$OUTPUT_DIR/output.json"
 mkdir -p "$OUTPUT_DIR"  # Ensure the directory exists
+
+if [ "$USE_PEFT" = true ]; then
+    echo "Using PEFT..."
+    PEFT_PATH="$MODEL_PATH"
+else
+    PEFT_PATH=""
+fi
 
 python src/sft_inference.py \
   --model_path "$MODEL_PATH"\
@@ -24,7 +31,7 @@ python src/sft_inference.py \
   --model_class "$MODEL_CLASS" \
   --model_family "$MODEL_FAMILY" \
   --max_length 512 \
-  --batch_size 4 \
+  --batch_size 2 \
   --slice_test -1 \
   --output_file "$OUTPUT_FILE" \
   --control_tokens "data/prompts/control_tokens.json" \
@@ -32,5 +39,5 @@ python src/sft_inference.py \
   --user_prompts "data/prompts/user_prompts.json" \
   --metric_mapping "data/metric_mapping.json" \
   --metric_name "$METRIC_NAME" \
-  --user_prompt_id "$USER_PROMPT_ID"
+  --user_prompt_id "$USER_PROMPT_ID"\
 
