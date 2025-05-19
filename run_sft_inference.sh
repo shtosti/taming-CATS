4a0323fb6a288b5d2254b51565e00168d119b31c
shtosti <hanna.hubarava@gmail.com>
2025-05-19 09:13:10 +0200

    delete unfinished models

--- run_sft_inference.sh
+++ run_sft_inference.sh
@@ -3,14 +3,10 @@
 
 # ========== inference =========
 USE_PEFT=false
-MODEL_PATH="models/Llama-3.2-1B-Instruct-Med-EASi_hq-ARI-base-token_explanation-20250517-1018-w8s135mh"
-USER_PROMPT_ID="token_explanation"
+MODEL_PATH="models/Llama-3.2-1B-Instruct-Med-EASi_hq-ARI-token_explanation-20250518-220417"
 METRIC_NAME="ARI"
-DATASET_NAME="Med-EASi_hq"
-# "llama", "mistral", "qwen", "base" (for prompt template)
-MODEL_FAMILY="base"
-# "llama" or "auto"
-MODEL_CLASS="llama"
+DATASET_NAME="Med-EASi"
+MODEL_CLASS="llama" # "llama" or "auto"
 
 
 OUTPUT_DIR="output/$MODEL_PATH"
@@ -18,20 +14,21 @@ OUTPUT_FILE="$OUTPUT_DIR/output.json"
 mkdir -p "$OUTPUT_DIR"  # Ensure the directory exists
 
 ARGS=(
+  --use_vllm
   --model_path "$MODEL_PATH"
   --dataset_name "$DATASET_NAME"
   --model_class "$MODEL_CLASS"
-  --model_family "$MODEL_FAMILY"
+  --model_family "base"
   --max_length 512
   --batch_size 4
-  --slice_test 10
+  --slice_test 5
   --output_file "$OUTPUT_FILE"
   --control_tokens "data/prompts/control_tokens.json"
   --system_prompts "data/prompts/system_prompts.json"
   --user_prompts "data/prompts/user_prompts.json"
   --metric_mapping "data/metric_mapping.json"
   --metric_name "$METRIC_NAME"
-  --user_prompt_id "$USER_PROMPT_ID"
+  --user_prompt_id "token_explanation"
 )
 
 if [ "$USE_PEFT" = true ]; then
