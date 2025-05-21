11d45ee70897e6e950ee92fa809437f7992759d5
shtosti <hanna.hubarava@gmail.com>
2025-05-21 11:17:36 +0200

    generate small newsela splits

--- run_flatten_splits.sh
+++ run_flatten_splits.sh
@@ -9,5 +9,11 @@ nvidia-smi
 
 # Run your Python script
 echo "Running the script ./src/flatten_splits.py ..."
-python ./src/flatten_splits.py
+
+
+python ./src/flatten_splits.py\
+    --keep_fraction "0.4"\
+    --seed "42"\
+    --dataset "Newsela"
+
 echo "Script finished."
